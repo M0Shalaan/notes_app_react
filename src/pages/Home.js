4f6de540f2abea90cdfae06b2fbf269e675edc5e
hungr@Mohamed-PC <mohamedshalaan67@gmail.com>
2023-01-22 23:48:13 +0200

    complete NoteList

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import AddNote from "../components/AddNote";
 import Header from "../components/Header";
 import Note from "../components/Note";
+import NotesList from "../components/NotesList";
 // constants for app
 const Home = () => {
 
@@ -12,7 +13,13 @@ const Home = () => {
     <div>
       <Header />
       <AddNote />
-      <Note />
+      {/* <NotesList
+					notes={notes.filter((note) =>
+						note.text.toLowerCase().includes(searchText)
+					)}
+					handleAddNote={addNote}
+					handleDeleteNote={deleteNote}
+				/>       */}
     </div>
   );
 };
