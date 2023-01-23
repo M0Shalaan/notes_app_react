24d69c99f51f980e7b71632f9653c69afdec19f1
hungr@Mohamed-PC <mohamedshalaan67@gmail.com>
2023-01-23 02:35:04 +0200

    added img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,41 +1,21 @@
 import React from "react";
-
+import note from "./images/note.png";
 const Header = ({ handleToggleDarkMode }) => {
   return (
     <div className="header">
-      <h1>Notes App</h1>
+      <h1>
+        Notes App <img src={note} alt="note " className="header-img" />{" "}
+      </h1>
       <button
-      className="save"
-        onClick={() =>
-          handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
-        }
-       >Toggle Mode</button>
-    </div>
-  );
-};
-export default Header;
-
-
-/* 
-
-import React from "react";
-
-const Header = ({ handleToggleDarkMode }) => {
-  return (
-    <div className="header">
-      <h1>Notes</h1>
-      <button
-        onClick={() =>
-          handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
-        }
         className="save"
+        onClick={() =>
+          handleToggleDarkMode((previousDarkMode) => !previousDarkMode)
+        }
       >
         Toggle Mode
       </button>
     </div>
   );
 };
-
 export default Header;
 
-*/
